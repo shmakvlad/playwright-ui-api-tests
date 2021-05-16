3e42a423869b124f53e1a1803f99eec0f81aa6ba
Shmakov Vlad <shmakvlad@gmail.com>
2021-05-16 23:58:07 +0300

    a little bit train

---
+++ js/destructuring.js
@@ -0,0 +1,44 @@
+// Array
+function calcValues(a, b) {
+    return [
+      a + b,
+      a - b,
+      a * b,
+      a / b
+    ]
+  }
+  
+const [sum, sub = 'Вычитания нет', mult, ...other] = calcValues(42, 10)
+// const [sum, , mult] = calcValues(42, 10)
+console.log(sum, mult, other, sub)
+  
+  
+// Objects
+const person = {
+name: 'Max',
+age: 20,
+address: {
+    country: 'Russia',
+    city: 'Moscow'
+}
+}
+
+
+// const name = person.name
+const {
+name: firstName = 'Без имени',
+age,
+car = 'Машины нет',
+address: {city: homeTown, country}
+} = person
+console.log(firstName, age, car, homeTown, country)
+
+const {name: title, ...info} = person
+console.log(title, info)  
+
+// Function
+function logPerson({name: firstName = '111', age}) {
+    console.log(firstName + ' ' + age)
+}
+  
+logPerson(person)
--- js/rest.js
+++ js/rest.js
@@ -8,10 +8,9 @@ const numbers = [1, 2, 3, 5, 8, 13]
 console.log(sum(1, 2, 3, 4, 5))
 console.log(sum(...numbers))                // Spread
 
-const [a, b, ...others] = numbers           // Spread
+const [a, b, ...others] = numbers           // Rest
 console.log(a, b, others)
 console.log(a, b, ...others)    
-
 
 // Object
 const son = {
@@ -22,21 +21,19 @@ const son = {
 }
 console.log(son)
 console.log(Object.entries(son))
-console.log(...Object.entries(son))
+console.log(...Object.entries(son)) // Spread
 
-const {n, ag} = son
-console.log(n, a)
+const {job, city, country} = son
+console.log(job, city, country)
 
-const {name, age, ...contacts} = son
+const {name, age, ...contacts} = son    // Rest
 console.log(name, age, contacts)
 console.log(name, age, Object.values(contacts))
 console.log(name, age, ...Object.values(contacts))
 
-
-
 // Practice
 Object.keys(contacts).forEach(key => {
-    if(contacts.hasOwnProperty(key)){
+    if(contacts.hasOwnProperty(key)){ 
         console.log(contacts[key])
     }
 })
